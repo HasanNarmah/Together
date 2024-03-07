6c9047e2e5a03e8a4ebc9870515b6d0f14469721
Hasan Narmah <Narmah.Hasan@gmail.com>
2024-03-07 14:07:27 +0000

    Initial Commit

---
+++ Together/TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogetherApp.swift
+//  Together
+//
+//  Created by Hasan Narmah on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
