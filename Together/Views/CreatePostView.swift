70e57ac353f9bf558b2056c7311a9c44ebb97807
Hasan Narmah <Narmah.Hasan@gmail.com>
2024-04-23 15:57:07 +0100

    Update README.md

--- Together/Views/CreatePostView.swift
+++ Together/Views/CreatePostView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CreatePostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("This is the view that will show how to post a post on Together")
     }
 }
 
